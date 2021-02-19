bb3f6897691e0933f9d278d61911428f3ce5e466
Spiridonovas,Edgaras E <e.spiridonovas@student.fontys.nl>
2021-02-19 16:11:52 +0100

    Components & UI using MaterializeCSS

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = props => {
+  return (
+    <div className='progress vlue lighten-4'>
+      <div className='indeterminate blue'></div>
+    </div>
+  );
+};
+
+export default Preloader;
---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import TechItem from "./TechItem";
+
+const TechListModal = () => {
+  const [techs, setTechs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getTechs = async () => {
+    setLoading(true);
+    const res = await fetch("/techs");
+    const data = await res.json();
+    // Timeout to see preloader
+    // setTimeout(() => {
+    setTechs(data);
+    setLoading(false);
+    // }, 2000);
+  };
+
+  useEffect(() => {
+    getTechs();
+  }, []);
+
+  return (
+    <div id='tech-list-modal' className='modal'>
+      <div className='modal-content'>
+        <h4>Technician List</h4>
+        <ul className='collection'>
+          {!loading &&
+            techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TechListModal;
